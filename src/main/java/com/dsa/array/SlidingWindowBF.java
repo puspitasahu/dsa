224ebe91a64a2294d73ac13ba1f238885d554e25
Sahu, Puspita <Puspita.Sahu@staples.com>
2026-01-30 00:43:56 -0500

    Array-Sliding Window Brute Force

---
+++ src/main/java/com/dsa/array/SlidingWindowBF.java
@@ -0,0 +1,19 @@
+package com.dsa.array;
+
+public class SlidingWindowBF{
+    public static void main(String[] args){
+        int[] nums = {2,1,5,8,9,4,6};
+        int k=3;
+        slideWindow(nums,k);
+    }
+    private static void slideWindow(int[] nums,int k){
+        int m = nums.length-k;//7-3=4
+        for(int i=0;i<=m;i++){
+            int sum =0;
+            for(int j=i;j<i+k;j++){
+                sum+=nums[j];
+            }
+            System.out.println(sum);
+        }
+    }
+}
